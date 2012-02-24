a5c5ddf3eab572947f300e13dbe81f4c81104974
Andrew Krowczyk <andrew@nin9creative.com>
2012-02-23 19:41:19 -0600

    Changes
    Modified icons.
    
    Signed-off-by: Andrew Krowczyk <andrew@nin9creative.com>

---
+++ Classes/shelf/InAppPurchaseManager.h
@@ -0,0 +1,13 @@
+//
+//  InAppPurchaseManager.h
+//  Baker
+//
+//  Created by Andrew Krowczyk on 2/22/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface InAppPurchaseManager : NSObject
+
+@end
---
+++ Classes/shelf/InAppPurchaseManager.m
@@ -0,0 +1,13 @@
+//
+//  InAppPurchaseManager.m
+//  Baker
+//
+//  Created by Andrew Krowczyk on 2/22/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "InAppPurchaseManager.h"
+
+@implementation InAppPurchaseManager
+
+@end
